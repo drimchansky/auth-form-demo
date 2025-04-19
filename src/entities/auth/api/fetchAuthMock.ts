9e720e1e31feda576f5496148e353b489db79053
Nikita Chernov <drimchansky@gmail.com>
2025-04-19 10:45:32 +0400

    Update fetch error handling

--- src/entities/auth/api/fetchAuthMock.ts
+++ src/entities/auth/api/fetchAuthMock.ts
@@ -3,9 +3,7 @@ import { UserData } from '../model/store'
 export const fetchAuthMock = (email: string, _password: string, delay = 4000, isError = false): Promise<UserData> => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (!navigator.onLine) {
-        reject(new Error('You are offline'))
-      } else if (isError) {
+      if (!navigator.onLine || isError) {
         reject(new Error('Something went wrong'))
       } else {
         resolve({ id: '1', email, name: 'Test User' })
